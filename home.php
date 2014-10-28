dcdb08d23185c5c5736749990e25a7f67c314555
Michael Espinosa <maespinosa9@misena.edu.co>
2014-10-28 15:55:24 -0500

    ...

--- home.php
+++ home.php
@@ -2,16 +2,16 @@
   $perusu = isset($_SESSION["perfil"]) ? $_SESSION["perfil"]:NULL;?>
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml">
-	  <head>
-		  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-				  <link rel="stylesheet" href="css/style.css" type="text/css" />
-		 		<link rel="stylesheet" href="css/estilos.css" type="text/css" />
-		  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js' type='text/javascript'/></script>
-	  </head>
-	  <body>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+          <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <link rel="stylesheet" href="css/estilos.css" type="text/css" />
+      <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js' type='text/javascript'/></script>
+    </head>
+    <body>
        <div id="contenedor">
-	  <div id="cabez">
-					</div>
+    <div id="cabez">
+          </div>
                     <div id="inicio">&nbsp;&nbsp;<a id="ini"href="home.php"><img border=0 src="image/home.png" width="30" height="30"/></a></div>
                     <div id="salir"><img border=0 src="image/salir1.png" width="30" height="30"/></div>
                       <div id="contenidorc">
@@ -27,7 +27,7 @@
                             include("vista/vproveedor.php");
                        }else if ($Pac == "102") {
                             include("vista/vtienda.php");
-								       } else if ($Pac == "103") {
+                       } else if ($Pac == "103") {
                             if (is_null($Up)) {
                                 include("vista/vfactura.php");
                             } else {
@@ -46,10 +46,10 @@
                         ?>
 
                     </div>
-				</div>
+        </div>
                 </div>
-				  <div id="pie">
-				   </div>
-				</div>		
-		  </body>
+          <div id="pie">
+           </div>
+        </div>    
+      </body>
   </html>
